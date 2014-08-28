5c8c3e9d0304a113f03a87f6e6935a19fef89722
rajeev <mca.rajeev@yahoo.in>
2014-08-28 22:31:41 +0530

    sql

---
+++ one2one/src/main/resources/export.sql
@@ -0,0 +1,38 @@
+--------------------------------------------------------
+--  File created - Thursday-August-28-2014   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table EMPLOYEE
+--------------------------------------------------------
+
+  CREATE TABLE "TEST"."EMPLOYEE" 
+   (	"EMPNO" NUMBER, 
+	"USERNAME" VARCHAR2(20 BYTE), 
+	"DEPTNO" NUMBER
+   ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
+  TABLESPACE "USERS" ;
+REM INSERTING into TEST.EMPLOYEE
+SET DEFINE OFF;
+Insert into TEST.EMPLOYEE (EMPNO,USERNAME,DEPTNO) values (1,null,null);
+Insert into TEST.EMPLOYEE (EMPNO,USERNAME,DEPTNO) values (2,null,null);
+Insert into TEST.EMPLOYEE (EMPNO,USERNAME,DEPTNO) values (3,null,null);
+--------------------------------------------------------
+--  DDL for Index SYS_C008627
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "TEST"."SYS_C008627" ON "TEST"."EMPLOYEE" ("EMPNO") 
+  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
+  TABLESPACE "USERS" ;
+--------------------------------------------------------
+--  Constraints for Table EMPLOYEE
+--------------------------------------------------------
+
+  ALTER TABLE "TEST"."EMPLOYEE" ADD PRIMARY KEY ("EMPNO")
+  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
+  TABLESPACE "USERS"  ENABLE;
---
+++ one2one/src/main/resources/export1.sql
@@ -0,0 +1,23 @@
+--------------------------------------------------------
+--  File created - Thursday-August-28-2014   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table PERSONALDETAILS
+--------------------------------------------------------
+
+  CREATE TABLE "TEST"."PERSONALDETAILS" 
+   (	"EMPNO" NUMBER, 
+	"FIRSTNAME" VARCHAR2(20 BYTE), 
+	"LASTNAME" VARCHAR2(20 BYTE)
+   ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
+  TABLESPACE "USERS" ;
+REM INSERTING into TEST.PERSONALDETAILS
+SET DEFINE OFF;
+--------------------------------------------------------
+--  Ref Constraints for Table PERSONALDETAILS
+--------------------------------------------------------
+
+  ALTER TABLE "TEST"."PERSONALDETAILS" ADD CONSTRAINT "EMPNO" FOREIGN KEY ("EMPNO")
+	  REFERENCES "TEST"."EMPLOYEE" ("EMPNO") ENABLE;
